f1b3cab810fc6337a2b0d5ec8be452d64c30a5f2
Kirill Gagarski <kirill.gagarski@gmail.com>
2017-05-06 00:17:37 +0300

    more configs

---
+++ configs/polyana.py
@@ -0,0 +1,28 @@
+import os
+
+from configs.util import from_data
+from operations.base import DeployList
+from operations.combine_files import GenerateGitConfigFromChunks, GenerateHgRcFromChunks
+from operations.directory import DeployDirectory, DeployDirectoryIfExists
+from operations.git import DeployGitRepo, DeployFilesFromGitRepo
+
+
+class Operation(DeployList):
+    operations = [
+        DeployGitRepo(
+            repo="https://github.com/robbyrussell/oh-my-zsh.git",
+            dst=".oh-my-zsh",
+        ),
+        DeployFilesFromGitRepo(
+            repo="https://github.com/gagarski/bullet-train-oh-my-zsh-theme.git",
+            dst=".oh-my-zsh/custom",
+            file_list=("bullet-train.zsh-theme",)
+        ),
+        DeployDirectory(from_data("main")),
+        DeployDirectory(from_data("work")),
+        DeployDirectory(from_data("linux")),
+        DeployDirectory(from_data("polyana")),
+        DeployDirectoryIfExists(from_data("private")),
+        GenerateGitConfigFromChunks(),
+        GenerateHgRcFromChunks()
+    ]
