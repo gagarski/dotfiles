f1f4aa7bb9ce4cac561374a7358a7c72fd57eb61
Kirill Gagarski <kirill.gagarski@gmail.com>
2017-05-06 00:29:18 +0300

    m

--- dfd.py
+++ dfd.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
-import os
 
 import importlib.util
 
 import sys
-
-BASE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/test")
 
 
 def run_from_file(path):
