933430ed3ca368c78812145ca264ba49b8f65c80
Kirill Gagarski <kirill.gagarski@gmail.com>
2017-05-05 22:51:21 +0300

    DeployList

--- operations/base.py
+++ operations/base.py
@@ -16,3 +16,24 @@ class Deploy:
 
     def run(self):
         raise NotImplementedError("Override me")
+
+
+class DeployList(Deploy):
+    operations = []
+    home = default_home
+
+    def __init__(self, propagate_home=False):
+        super(DeployList, self).__init__(self.home)
+        self.propagate_home = propagate_home
+
+    def run(self):
+        for op in self.operations:
+            old_home = None
+
+            if self.propagate_home:
+                old_home = op.home
+
+            op.run()
+
+            if self.propagate_home:
+                op.home = old_home
